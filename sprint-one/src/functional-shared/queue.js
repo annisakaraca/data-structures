d33f47551b5e166173601cb82365dd0ced4a8d7a
Annisa <annisakaraca@gmail.com>
2018-01-17 15:50:46 -0800

    Make the queue tests pass for the functional-shared pattern

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = {};
+
+  newQueue.count = 0;
+  newQueue.storage = {};
+  newQueue.firstIndex = 0;
+
+  _.extend(newQueue, queueMethods);
+
+  return newQueue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: function() {
+    return (this.count - this.firstIndex);
+  },
+  enqueue: function(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  },
+  dequeue: function() {
+    if ((this.count - this.firstIndex) === 0) {
+      return undefined;
+    } else {
+      var dequeuedValue = this.storage[this.firstIndex];
+      this.firstIndex++;
+      return dequeuedValue;
+    }  
+  }
+};
 
 

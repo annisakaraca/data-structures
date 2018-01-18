9abb3ad703d493654a61f7217ace3378e9a28140
Annisa <annisakaraca@gmail.com>
2018-01-17 16:08:04 -0800

    Make queue tests pass for the prototypal instantiation pattern

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,32 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = Object.create(queueMethods);
+
+  newQueue.storage = {};
+  newQueue.count = 0;
+  newQueue.firstIndex = 0;
+
+  return newQueue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  },
+  dequeue: function() {
+    if (this.count - this.firstIndex === 0) {
+      return undefined;
+    } else {
+      var dequeuedValue = this.storage[this.firstIndex];
+      this.firstIndex++;
+      return dequeuedValue;
+    }
+  },
+  size: function() {
+    return (this.count - this.firstIndex);
+  }
+};
 
 

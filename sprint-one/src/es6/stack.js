56203275b92d7f2d83ba1c312d66487ee60e2a78
Annisa <annisakaraca@gmail.com>
2018-01-17 17:38:04 -0800

    Make stack tests pass for the ES6 instantiation pattern

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,25 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.count = 0;
+    this.storage = {};
   }
 
+  push(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  }
+
+  pop() {
+    if (this.count === 0) {
+      return undefined;
+    } else {
+      this.count--;
+      return (this.storage[this.count]);
+    }
+  }
+
+  size() {
+    return this.count;
+  }
 }

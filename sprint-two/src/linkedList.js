19ebb94df349a1127354a88a448f68e28a00f94a
annisakaraca <annisakaraca@gmail.com>
2018-01-18 10:52:52 -0800

    Make linkedList tests pass

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,32 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var addedNode = Node(value);
+    if (list.head === null) {
+      list.head = addedNode;
+    } else {
+      list.tail.next = addedNode;
+    }
+    list.tail = addedNode;
   };
 
   list.removeHead = function() {
+    var item = list.head.value;
+    list.head = list.head.next;
+    return item;
   };
 
   list.contains = function(target) {
+    var search = function(node, target) {
+      if (node.value !== target && node.next === null) {
+        return false;
+      } else if (node.value === target) {
+        return true;
+      } else {
+        return search(node.next, target);
+      }
+    };
+    return search(list.head, target);
   };
 
   return list;

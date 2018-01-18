6c56f3a36de2aaa109b26c2674878fd7ac9d75d5
Annisa <annisakaraca@gmail.com>
2018-01-17 17:45:18 -0800

    Make queue tests pass for the ES6 instantiation pattern

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,27 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = 0;
+    this.firstIndex = 0;
   }
 
+  enqueue(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  }
+
+  dequeue() {
+    if (this.count - this.firstIndex === 0) {
+      return undefined;
+    } else {
+      var dequeuedValue = this.storage[this.firstIndex];
+      this.firstIndex++;
+      return dequeuedValue;
+    }
+  }
+
+  size() {
+    return (this.count - this.firstIndex);
+  }
 }
